3f43278a814a772ea3586625ed8be17aacc3c58a
ptrvenckus <ptrvenckus@gmail.com>
2022-11-20 11:51:37 +0100

    feat: add delivery script and style files

--- pages/main/script.js
+++ pages/main/script.js
@@ -16,11 +16,29 @@ container.append(footer);
 const heading = document.createElement('h1');
 header.append(heading);
 heading.innerText = "Welcome to the amazing book shop!";
+heading.setAttribute('class', 'heading');
 
 body.setAttribute('class', 'body');
 header.setAttribute('class', 'header');
 main.setAttribute('class', 'main');
 footer.setAttribute('class', 'footer');
+
+const books = document.createElement('div');
+const booksHeading = document.createElement('h2');
+booksHeading.innerText = "Available books";
+books.append(booksHeading);
+main.append(books);
+
+const bag = document.createElement('div');
+const bagHeading = document.createElement('h2');
+bagHeading.innerText = "Bag for your chosen books";
+bag.append(bagHeading);
+main.append(bag);
+
+const link = document.createElement('a');
+link.href = "../delivery/index.html";
+link.innerText = "Delivery Page";
+footer.append(link);
 
 docFragment.append(container);
 body.append(docFragment);
@@ -32,15 +50,17 @@ fetch('../books.json', { mode: 'no-cors' }).then(response => {
   for (let i = 0; i < data.length; i++) {
     bookContainer[i] = document.createElement('div');
     bookContainer[i].setAttribute('class', 'book-container');
-    main.append(bookContainer[i]);
-
+    books.append(bookContainer[i]);
+    
+    books.addEventListener('click', (event) => {
+      bag.append(bookContainer[0]);
+    });
+    
     const author = document.createElement('h2');
     author.innerText = data[i].author;
     bookContainer[i].append(author);
 
     const image = document.createElement('img');
-    image.setAttribute('class', 'src');
-    image.setAttribute('class', 'alt');
     image.src = data[i].imageLink;
     image.alt = `book ${i} image`;
     bookContainer[i].append(image);
@@ -55,11 +75,30 @@ fetch('../books.json', { mode: 'no-cors' }).then(response => {
 
     const description = document.createElement('p');
     description.innerText = data[i].description;
+    description.setAttribute('class', 'description');
     bookContainer[i].append(description);
 
+    // const popup = document.createElement('div');
+    // const closeBtn = document.createElement('button');
+    // const popupBody = document.createElement('div');
+    // closeBtn.innerText = "x";
+    // closeBtn.setAttribute('class', 'close-button');
+    // popupBody.innerText = data[i].description;
+    // popup.append(closeBtn);
+    // popup.append(popupBody);
+    // bookContainer[i].append(popup);
+    
+    const showMore = document.createElement('button');
+    showMore.innerText = 'Show more';
+    showMore.setAttribute('class', 'show-more-btn');
+    bookContainer[i].append(showMore);
+
+    const addToBag = document.createElement('button');
+    addToBag.innerText = "Add to Bag";
+    addToBag.setAttribute('class', 'add-to-bag-btn');
+    bookContainer[i].append(addToBag);
   }
 });
-
 
 
 
